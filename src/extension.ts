85c8424dfe4ab74947f604694101905cad40aec7
Recordum <mingyu.oh15b@gmail.com>
2023-12-11 21:39:55 +0900

    fix: vscode engine version fix

--- src/extension.ts
+++ src/extension.ts
@@ -1,7 +1,6 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
@@ -17,7 +16,9 @@ export function activate(context: vscode.ExtensionContext) {
     () => {
       // The code you place here will be executed every time your command is executed
       // Display a message box to the user
-      vscode.window.showInformationMessage('Hello World from NaminGenie!');
+      vscode.window.showInformationMessage(
+        'Hello World from NaminGenie!!!!!!!!',
+      );
     },
   );
 
